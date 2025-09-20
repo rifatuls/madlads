4dca69ee2e0e00d8e707b600351b7b9e2605d505
Mohd. Rifatul Maksud (Manager <rifatuls@gmail.com>
2025-09-20 21:24:55 +0600

    Going for the Markdown

--- v1/workflows/bot_test_x.py
+++ v1/workflows/bot_test_x.py
@@ -1,10 +1,10 @@
 import tweepy
 
 auth = tweepy.OAuth1UserHandler(
-    consumer_key="tfQeSQmY5rg4m6tbue9KlwCl2",
-    consumer_secret="6iaEmk0VxuX6OLsoJG5VIcV300qlOwHKuYTI9rwbntaoNa8NYI",
-    access_token="106975972-y88v7fmzQL7vr77nmuvY2TpJJkRNlEN8PJvkPl1h",
-    access_token_secret="eDKiJJJbOdHYVyjIZnw4XUmo6teTzJ1jK6kNLkQulAAhf"
+    consumer_key="",
+    consumer_secret="",
+    access_token="",
+    access_token_secret=""
 )
 
 api = tweepy.API(auth)
